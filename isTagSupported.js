27da414479c272e23d6c0dd3f132fac271d95fca
Ryan Morr <rm.morr@gmail.com>
2013-12-07 16:23:49 -0500

    Updated conventions

--- isTagSupported.js
+++ isTagSupported.js
@@ -1,27 +1,27 @@
 /*
  * isTagSupported
  * Feature test HTML tag support 
- * @param {String} tag The tag being tested
- * @return {Boolean} True or false based on results of the test
+ * @param {String} tag
+ * @return {Boolean}
  */
 
 (function(win){
-	"use strict";		
+	'use strict';		
 	
-	//Define common variables
+	// Define common variables
 	var cache = {}, doc = win.document, objectToString = win.Object.prototype.toString;
 	
-	//Define the function
+	// Define the function globally
 	win.isTagSupported = function(tag){
-		//Check the cache
+		// Check the cache
 		if(tag in cache){
-			//If the tag is found in the cache, return the result
+			// If the tag is found in the cache, return the result
 			return cache[tag];	
 		}
-		//Create a test element for the tag
+		// Create a test element for the tag
 		var element = doc.createElement(tag);
-		//Obtain the element's internal [[Class]] property, if it doesn't match the HTMLUnknownElement interface than it must be supported
-		//Cache and return the result
+		// Obtain the element's internal [[Class]] property, if it doesn't match the HTMLUnknownElement 
+		// interface than it must be supported, then cache and return the result
 		return cache[tag] = (objectToString.call(element) !== '[object HTMLUnknownElement]');
 	};
 	
