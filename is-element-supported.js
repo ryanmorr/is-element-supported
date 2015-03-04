dc362eb021eea475ed029679368d5e8bc13d82e6
Ryan Morr <rm.morr@gmail.com>
2015-03-04 15:46:33 -0500

    Refactored

--- is-element-supported.js
+++ is-element-supported.js
@@ -14,7 +14,7 @@
         // Create a test element for the tag
         var element = document.createElement(tag);
         // Obtain the element's internal [[Class]] property, if it doesn't 
-        // match the HTMLUnknownElement interface than it must be supported
+        // match the `HTMLUnknownElement` interface than it must be supported
         return toString.call(element) !== '[object HTMLUnknownElement]';
     };
     
