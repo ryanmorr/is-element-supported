cb9b4a071b87ca679208d60175d9df0ffb1d7dd4
Ryan Morr <rm.morr@gmail.com>
2019-02-01 20:06:59 -0500

    Build v0.1.0

---
+++ dist/is-element-supported.js
@@ -0,0 +1,25 @@
+/*! @ryanmorr/is-element-supported v0.1.0 | https://github.com/ryanmorr/is-element-supported */
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.isElementSupported = f()}})(function(){var define,module,exports;return (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = isElementSupported;
+
+/*
+ * Feature test HTML element support
+ *
+ * @param {String} tag
+ * @return {Boolean|Undefined}
+ */
+function isElementSupported(tag) {
+  var element = document.createElement(tag);
+  return {}.toString.call(element) !== '[object HTMLUnknownElement]';
+}
+
+module.exports = exports.default;
+
+},{}]},{},[1])(1)
+});
+
